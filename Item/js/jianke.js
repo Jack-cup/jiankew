e6bd969b1ea74842424761316076beb24e5bec7a
wanghui <1027332684@qq.com>
2019-10-12 20:14:03 +0800

    .

--- Item/js/jianke.js
+++ Item/js/jianke.js
@@ -1,10 +1,20 @@
 $(() => {
     //头部数据请求
     $.getJSON("../server/jk_head_top.json", (data) => {
-        console.log(data);
 
         let oUl = renderHeaderTop(data)
         $(".jk_head_top_right>.jk_nav").append(oUl)
+    });
+    //nav请求数据
+    $.ajax({
+        type: "get",
+        url: "../server/nav.php",
+        dataType: "json",
+        success: function (response) {
+            // console.log(response);
+
+            $(".jk_banner_l").append(navRender(response))
+        }
     });
     //头部渲染
     function renderHeaderTop(data) {
@@ -19,22 +29,59 @@ $(() => {
         <li class="four"><p class="ul_list">商城</p>${html4}</li>
                 </ul>`
     }
+
+    //nav渲染
+    function navRender(data) {
+        var res = "";
+        for (var i = 0, len = data.length; i < len; i++) {
+            var a = JSON.parse(data[i].data1.replace(/^\"|\"$/g, ''))
+            var b = JSON.parse(data[i].data2.replace(/^\"|\"$/g, ''))
+            var c = JSON.parse(data[i].data3.replace(/^\"|\"$/g, ''))
+            var d = JSON.parse(data[i].data4.replace(/^\"|\"$/g, ''))
+            var html1 = a.map(function (key) {
+                return `<a href="">${key}</a>`
+            }).join("")
+            var html2 = b.map(function (key) {
+                return `<dd><a href="">${key}</a></dd>`
+            }).join("")
+            var html3 = c.map(function (key) {
+                return `<dd><a href="">${key}</a></dd>`
+            }).join("")
+            var html4 = d.map(function (key) {
+                return `<dd><a href=""><img src="${key}" alt=""></a></dd>`
+            }).join("")
+            // console.log(a.slice(0, 1));
+
+            res += ` <li class="nav_li">
+                             <div class="jk_list"><p class="lists">${html1}</p></div>
+                             <div class="jk_animate">
+                             <dl>
+                                  <dt>${a.slice(0, 1)}</dt>
+                                  ${html2}
+                             </dl>
+                             <dl>
+                                  <dt>推荐产品</dt>
+                                  ${html3}
+                             </dl>
+                             <dl>
+                                  <dt>推荐品牌</dt>
+                                  ${html4}
+                             </dl>
+                             </div>
+                         </li>`
+        }
+        return res
+    }
     //头部监听
     $(".list").hover(function () {
         $(this).children(".second-list").stop().slideToggle(200);
     })
+    //nav事件监听
+    $(".jk_banner_l").on("mousenter", ".jk_list", function () {
+        console.log(1);
 
-    ///////
-    $.ajax({
-        type: "get",
-        url: "../server/nav.php",
-        dataType: "json",
-        success: function (response) {
-            console.log(response);
-
-        }
-    });
-
+    })
+    console.log($(".jk_animate"));
 
 
 
--- Item/server/floor.php
+++ Item/server/floor.php
@@ -1,7 +1,7 @@
 <?php
 header("Content-type:text/html;charset=utf-8");
 $db=mysqli_connect("127.0.0.1","root","","jianke");
-$sql="SELECT * FROM nav";
+$sql="SELECT * FROM floor";
 mysqli_query($db, "set names 'utf8'");
 $result = mysqli_query($db, $sql);
 // mysqli_num_rows($result);
