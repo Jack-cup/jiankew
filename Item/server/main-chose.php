2f1a82f47de62aea58b4927aba8c25832c9cdfb0
wanghui <1027332684@qq.com>
2019-10-16 08:31:43 +0800

    ,

--- Item/server/main-chose.php
+++ Item/server/main-chose.php
@@ -1,9 +1,10 @@
 <?php
 header("Content-type:text/html;charset=utf-8");
 $db=mysqli_connect("127.0.0.1","root","","jianke");
-$sql="SELECT * FROM floor1";
+$sql="SELECT * FROM mainchose";
 mysqli_query($db, "set names 'utf8'");
 $result = mysqli_query($db, $sql);
+// mysqli_num_rows($result);
 $data = mysqli_fetch_all($result,MYSQLI_ASSOC);
 echo json_encode($data,true);
 ?>
